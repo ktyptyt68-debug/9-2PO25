298f20059353cd0edbbe764fda68c0f6dbdde6c4
ktypt <ktyptyt68@gmail.com>
2026-09-03 06:32:12 +0600

    added functions and refactor second sample

--- samples/secndsmpl.py
+++ samples/secndsmpl.py
@@ -1,13 +1,16 @@
 
+def function1(int1, int2, list1):
+    for i in range(1, 21):
+        print(i)
+        list1.append(i)
+    print("-"*20)
+    print(list1)
+
 while True:
     if int(input("нажмите 1 чтобы выйти")) == 1:
         break
     else:
         print("продолжаем...")
-list1 = []
-for i in range(1, 21):
-    print(i)
-    list1.insert(i, i)
 
-print("-"*20)
-print(list1)
+list1 = []
+function1(1, 21, list1)
