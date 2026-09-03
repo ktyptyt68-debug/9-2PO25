b0a1ffdd49ba5572f9a1dcc4bc711801a97e04ad
ktypt <ktyptyt68@gmail.com>
2026-09-03 06:05:16 +0600

    sample

---
+++ samples/frstsmpl.py
@@ -0,0 +1,14 @@
+question1 = int(input("select: "))
+if question1 == 1:
+    name="alex"
+    age=27
+    print("name: " + name)
+    print("age: " + age)
+elif question1 == 2:
+    print(2+2)
+    print(7*2)
+    print(8-3)
+    print(10/7)
+elif question1 == 3:
+    question2 = input(" как тебя зовут: ")
+    print("вас зовут: " + question2)
